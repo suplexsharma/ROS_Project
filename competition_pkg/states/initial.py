d1d0b5ba08224a1d07a30af2239e767fb42538e4
ros2 <ros2@tamROS2-2503.myguest.virtualbox.org>
2026-05-22 16:31:01 +0900

    initial state machine

---
+++ competition_pkg/states/initial.py
@@ -0,0 +1,17 @@
+import rclpy
+from rclpy.duration import Duration
+from rclpy.node import Node
+from yasmin import State
+from yasmin import Blackboard
+
+
+class InitialState(State):
+    """Initial state"""
+
+    def __init__(self, node: Node):
+        super().__init__(outcomes=["wait_gesture"])
+        self.node = node
+
+    def execute(self, blackboard: Blackboard) -> str:
+        self.node.get_logger().info(f"Executing state {self.__class__.__name__}")
+        return "wait_gesture"
